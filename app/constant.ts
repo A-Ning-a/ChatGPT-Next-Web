872a0811fdfc11fde790edee37f1170ef68104a9
岑福宁 <cenfuning@192.168.2.4>
2023-06-11 13:04:12 +0800

    fix: branch version difference

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,17 @@
 export const OWNER = "A-Ning-a";
+export const BRANCH: string = "custom";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const REPO_URL = `https://github.com/${OWNER}/${REPO}${
+  BRANCH === "main" || BRANCH === "master" ? "" : `/tree/${BRANCH}`
+}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1${
+  BRANCH === "main" || BRANCH === "master" ? "" : `&sha=${BRANCH}`
+}`;
+export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1${
+  BRANCH === "main" || BRANCH === "master" ? "" : `&sha=${BRANCH}`
+}`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
